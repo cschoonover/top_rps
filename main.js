eb2b877df92d377e3cedfb7813154ee482cde429
cschoonover <cortlandt.schoonover@gmail.com>
2021-05-26 16:13:00 -0500

    all of the game logic

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ function playRound(playerSelection, computerSelection)
     {
         return "You tied the computer!";
     }
-    else if((playerSelection == "Rock" && computerSelection == "Scissors") ||
-    (playerSelection == "Scissors" && computerSelection == "Paper") ||
-    (playerSelection == "Paper" && computerSelection == "Rock"))
+    else if((playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock"))
     {
         return "You win! " + playerSelection + " beats " + computerSelection;
     }
@@ -28,6 +28,55 @@ function playRound(playerSelection, computerSelection)
     }
 }
 
-const playerSelection = "scissors";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection,computerSelection));
+function determineWinner(playerWins, computerWins)
+{
+    if(playerWins>computerWins)
+    {
+        return "You have defeated the computer. " + playerWins + " to " + computerWins;
+    }
+    else if(computerWins > playerWins)
+    {
+        return "Predictably, the computer has defeated you " + computerWins + " to " + playerWins;
+    }
+    else
+    {
+        return "The stars have aligned. You have met your true equal adversary. Tie ballgame."
+    }
+}
+
+function testRPS()
+{
+    const playerSelection = "scissors";
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection,computerSelection));
+}
+
+function game(){
+    let playerWins = 0;
+    let computerWins = 0;
+    for(let i = 0 ; i<5 ; i++)
+    {
+        let playerSelection = prompt("Choose rock, paper, or scissors.");
+        let roundResult = playRound(playerSelection, computerPlay());
+        console.log(roundResult);
+        //bein' lazy
+        if(roundResult.includes("win"))
+        {
+            playerWins++;
+        }
+        else if(roundResult.includes("lose"))
+        {
+            computerWins++;
+        }
+        else 
+        {
+            //it was a tie, we assume and we aren't even counting those right now
+        }
+    }
+    //finally determine the winner
+    console.log(determineWinner(playerWins,computerWins));
+
+}
+
+//actually run the game loop
+game();
